dba09d8bf50da33a5f67d2581be7356165105542
arun279 <arun.km.279@gmail.com>
2017-11-19 16:19:01 -0500

    adding work related to USPS data retrieval

---
+++ usps_data.py
@@ -0,0 +1,47 @@
+import numpy as np
+import random as ran
+import tensorflow as tf
+import cv2
+import os
+import pandas as pd
+from IPython.display import Image
+
+'''
+In this code, I try to retrieve USPS data from a directory structure
+where name of the directort is the label for the files in that directory
+'''
+
+path = 'F:/UB/Sem 1/CSE574 - Intro to ML/Assignments/3/new_data/proj3_images/Numerals/'
+
+# Initialize 2 lists for labels and images
+imlist = []
+lblist = []
+dirs = list(os.walk(path))[0]
+dirs = dirs[1]
+for d in dirs:
+    #print(d)
+    for files in d:
+        directory = os.path.join(path,files)
+        #print(directory)
+        for fname in os.listdir(directory):
+            if(fname.endswith(".png")):    
+                image = cv2.imread(os.path.join(path,d,fname))
+                im_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+                im_resize = cv2.resize(im_gray, (28, 28))
+                imlist.append(np.ravel(im_resize))
+                lblist.append(int(d))
+
+# Normalize the values in the image
+images = np.array(imlist)/255.
+labels = np.reshape(np.array(lblist),[-1,1]).flatten()
+# Get one-hot representation
+labels = np.array(pd.get_dummies(labels))
+
+# Set a random seed to get reproducible results
+np.random.seed(42)
+
+# Randomize the input data
+ids = list(range(0,len(labels)))
+np.random.shuffle(ids)
+images_shuf = images[ids]
+labels_shuf = labels[ids]
